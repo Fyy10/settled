fe21686413b60bcef2c6c956f22ff49a76d42577
Jeff Fu <jefffu233@gmail.com>
2026-02-04 17:31:14 -0800

    shadcn: add component Button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+<div class="flex gap-4 mt-4">
+	<Button>Default Button</Button>
+	<Button variant="destructive">Destructive</Button>
+	<Button variant="outline">Outline</Button>
+	<Button variant="secondary">Secondary</Button>
+	<Button variant="ghost">Ghost</Button>
+	<Button variant="link">Link</Button>
+</div>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
